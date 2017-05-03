daaf4dec042b5fff6e3c0fc62b02f57082e8e3c4
fintan12 <fintan12@gmail.com>
2017-05-03 14:55:26 +1200

    preference panels added

--- resources/js/script.js
+++ resources/js/script.js
@@ -31,3 +31,23 @@ function getData() {
 
   $('.output-text').html("Range has been set to" + input);
 }
+
+
+$(document).ready(function() {
+   $('select').material_select();
+ });
+
+
+ var slider = document.getElementById('test5');
+  noUiSlider.create(slider, {
+   start: [20, 80],
+   connect: true,
+   step: 1,
+   range: {
+     'min': 0,
+     'max': 100
+   },
+   format: wNumb({
+     decimals: 0
+   })
+  });
